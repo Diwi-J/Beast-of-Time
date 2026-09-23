74aba3340a2912afddd80892d2f7b8797d820fc4
LinkyNzuza <linky.nzuza07@gmail.com>
2026-09-23 09:52:48 +0200

    Last changes for Milestone 1

--- Assets/Project/Scripts/Combat/Characters/CharacterCombat.cs
+++ Assets/Project/Scripts/Combat/Characters/CharacterCombat.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 using System.Collections.Generic;
 using UnityEngine.InputSystem;
 using UnityEditor;
-using UnityEditor.EditorTools;
 using Unity.Collections.Tests.CoreCLR.TestJobs;
 
 public class CharacterCombat : MonoBehaviour
